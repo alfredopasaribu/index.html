52ddbe5fec379cc80ae165e256b48ed4d385ff10
unknown <alfredopasaribu69@gmail.com>
2020-05-24 12:40:12 +0700

    fris commit

---
+++ scrip.js
@@ -0,0 +1,18 @@
+function ganti(angka) {
+            var lokasi = 'img/' + angka + '.svg';
+          var gant = document.getElementById('imggg').src = lokasi;
+            return false;
+            
+        }
+
+
+// function back() {
+// 	var gan =document.getElementById('imges');
+// 	gan.style.backgroundImage='Anonymous_House_1.svg';
+// }
+
+// function next() {
+// 	var next =document.getElementById('imges');
+// 	next.style.backgroundImage='SCH05.svg';
+
+// }
